3ff138657c667bd2c5ba08f5e90f0c489e6b1662
Rahul Roy <rahulroy.ec21@rvce.edu.in>
2024-01-27 16:33:32 +0530

    portfolio

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StickyNavbar } from '../components/shared/Navbar'
+
+const Home = () => {
+  return (
+    <div className="mt-4 mx-auto">
+       <StickyNavbar/>
+    </div>
+  )
+}
+
+export default Home
+
